ce647cd2c4c62218a2778ccd338b7c6da8defb89
João Vinagre <joaoms@joaoms.com>
2022-05-04 19:15:55 +0100

    Incomplete constructors

--- recommenders_implicit/RAISGD.py
+++ recommenders_implicit/RAISGD.py
@@ -9,8 +9,8 @@ class RAISGD(ISGD):
     Vinagre, J., Jorge, A. M., & Gama, J. (2015, April). Collaborative filtering with recency-based negative feedback. In Proceedings of the 30th Annual ACM Symposium on Applied Computing (pp. 963-965).
     https://dl.acm.org/doi/abs/10.1145/2695664.2695998
     """
-    def __init__(self, data: ImplicitData, num_factors: int, num_iterations: int, learn_rate: float, regularization: float, random_seed: int, ra_length: int):
-        super().__init__(data, num_factors, num_iterations, learn_rate, regularization, random_seed)
+    def __init__(self, data: ImplicitData, num_factors: int = 10, num_iterations: int = 10, learn_rate: float = 0.01, u_regularization: float = 0.1,    i_regularization: float = 0.1, random_seed: int = 1, ra_length: int = 1):
+        super().__init__(data, num_factors, num_iterations, learn_rate, u_regularization, i_regularization, random_seed)
         self.ra_length = ra_length
 
     def _InitModel(self):
--- recommenders_implicit/RSISGD.py
+++ recommenders_implicit/RSISGD.py
@@ -7,8 +7,8 @@ class RSISGD(ISGD):
     """
     
     """
-    def __init__(self, data: ImplicitData, num_factors: int, num_iterations: int, learn_rate: float, regularization: float, random_seed: int, ra_length: int = 1):
-        super().__init__(data, num_factors, num_iterations, learn_rate, regularization, random_seed)
+    def __init__(self, data: ImplicitData, num_factors: int = 10, num_iterations: int = 10, learn_rate: float = 0.01, u_regularization: float = 0.1, i_regularization: float = 0.1, random_seed: int = 1, ra_length: int = 1):
+        super().__init__(data, num_factors, num_iterations, learn_rate, u_regularization, i_regularization, random_seed)
         self.ra_length = ra_length
 
     def IncrTrain(self, user, item, update_users: bool = True, update_items: bool = True):
@@ -26,6 +26,6 @@ class RSISGD(ISGD):
             while negative_item_id in user_items:
                 negative_item_id = random.choice(self.data.items)
 
-            self._UpdateFactors(user_id, negative_item_id, target = 0)
+            self._UpdateFactors(user_id, negative_item_id, True, False, 0)
         
         self._UpdateFactors(user_id, item_id)
